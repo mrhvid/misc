8e2c40f02fdee8733fe8fff9efd8873e1f699e37
mrhvid <git@1d.dk>
2015-08-09 11:11:00 +0200

    Empty file.
    Starting to play with hyper-v / test setup

---
+++ Build-TestServerSetup.ps1
@@ -0,0 +1,2 @@
+﻿
+G:\temp\Convert-WindowsImage.ps1 -SourcePath h:\sources\install.wim -VHDPath G:\temp\CoreFromIso.vhdx -SizeBytes 20gb -VHDType Dynamic -VHDFormat VHDX -VHDPartitionStyle GPT -Edition 3 -Verbose
